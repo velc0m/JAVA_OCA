4e6c4f37904d755c2a6047ccae43206b25788734
Литвиненко Виталий Валерьевич <V.Litvinenko@bssys.com>
2015-11-26 19:21:43 +0300

    primitive cast

---
+++ OCA/src/Chapter_3/Primitive_cast/Test.java
@@ -0,0 +1,16 @@
+package Chapter_3.Primitive_cast;
+
+/**
+ * Created by Litvv on 26.11.2015.
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a;
+        System.out.println("b before = " + b);
+        a = 30;
+        System.out.println("b after - " + b);
+    }
+}
